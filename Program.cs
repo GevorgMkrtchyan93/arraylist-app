4594839e7f75f1a0c177b82e93fe5d017a7e4e82
Gev123 <gevorg.mkrtchyan@milies.net>
2021-10-28 17:15:19 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ArrayList arrayList = new _ArrayList();
+           
+            int count = arrayList.Count;
+            int capacity = arrayList.Capacity;
+            arrayList.Add(12);
+            arrayList.Insert(1,12);
+            arrayList.EnsureCapacity(1);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ _ArrayList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ArrayListApp
+{
+    class _ArrayList
+    {
+        private int[] _items;
+        private int _size;
+        private int _version;
+        private const int _defaultCapacity = 4;
+        static readonly int[] _emptyArray = new int[0];
+        public _ArrayList()
+        {
+            _items = _emptyArray;
+        }
+        public _ArrayList(int capacity)
+        {
+            if (capacity == 0)
+            {
+                _items = _emptyArray;
+            }
+            else
+            {
+                _items = new int[capacity];
+            }
+        }
+        public int Capacity
+        {
+            get { return _items.Length; }
+            set
+            {
+                if (value != _items.Length)
+                {
+                    if (value > 0)
+                    {
+                        int[] newItems = new int[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_items, 0, newItems, 0, _size);
+                        }
+                        _items = newItems;
+                    }
+                    else
+                    {
+                        _items = new int[_defaultCapacity];
+                    }
+                }
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+        public void Add(int value)
+        {
+            if (_size == _items.Length)
+                EnsureCapacity(_size + 1);
+            _items[_size] = value;
+            _version++;
+        }
+        public void Insert(int index, int value)
+        {
+            if (_size == _items.Length)
+                EnsureCapacity(_size + 1);
+            if (index < _size)
+            {
+                Array.Copy(_items, index, _items, index + 1, _size - index);
+            }
+            _items[index] = value;
+            _size++;
+            _version++;
+        }
+
+        public void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                int newCapacity = _items.Length == 0 ? _defaultCapacity : _items.Length * 2;
+                if ((uint)newCapacity > 0X7FEFFFFF)
+                {
+                    newCapacity = 0X7FEFFFFF;
+                }
+                if (newCapacity < min)
+                {
+                    newCapacity = min;
+                }
+                Capacity = newCapacity;
+            }
+        }
+    }
+}
